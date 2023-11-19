95dd0c428986dccc447e34ab0514cef94dc52e9c
Le0Bonhart <Le0Bonhart@yandex.ru>
2023-11-19 16:54:24 +1000

    Added constructors to Run & User

--- backend/Models/DataBase/Run.cs
+++ backend/Models/DataBase/Run.cs
@@ -6,8 +6,18 @@ namespace backend.Models.DataBase
     {
         public Guid Id { get; set; }
         public int Score { get; set; }
-        public string Date { get; set; }
+        public DateTime Date { get; set; }
         public Guid UserId { get; set; }
         public User User { get; set; }
+
+        protected Run(){}
+
+        public Run(Guid guid, int score, DateTime date, Guid userGuid)
+        {
+            Id = guid;
+            Score = score;
+            Date = date;
+            UserId = userGuid;
+        }
     }
 }
--- backend/Models/DataBase/User.cs
+++ backend/Models/DataBase/User.cs
@@ -5,9 +5,24 @@ namespace backend.Models.DataBase
     public class User
     {
         public Guid Id { get; set; }
+        [MaxLength(32)]
         public string Login {  get; set; }
-        [MaxLength(255)]
+        [MaxLength(32)]
         public string DisplayName { get; set; }
         public string Password { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+        protected User() {}
+        public User(Guid guid, string login, string display, string password, string refresh, DateTime refreshExp)
+        {
+            if (login.Length > 32 || display.Length > 32) 
+                throw new ArgumentOutOfRangeException();
+            Id = guid;
+            Login = login;
+            DisplayName = display;
+            Password = password;
+            RefreshToken = refresh;
+            RefreshTokenExpiryTime = refreshExp;
+        }
     }
 }
