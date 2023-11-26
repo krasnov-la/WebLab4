c72d13861fd2db07ea7a431c0db479aafe6a4ad4
Le0Bonhart <Le0Bonhart@yandex.ru>
2023-11-26 23:39:45 +1000

    PlayerBehavior

--- frontend/src/game/scenes/MainScene.ts
+++ frontend/src/game/scenes/MainScene.ts
@@ -1,30 +1,59 @@
 import { Scene } from "phaser";
+import { Types } from "phaser";
+import { Player } from "../objects/player";
+import { PlayerBullet } from "../objects/playerBullet";
+import { Bullet } from "../objects/bullet";
 
 export class MainScene extends Scene {
+    public _player : any;
+    public _projectiles : Bullet[] = [];
+    private _cursorKeys : Types.Input.Keyboard.CursorKeys | undefined;
+    private _ticks = 0;
+  
     constructor () {
-      super({ key: 'MainScene' })
-    }
-    
-    // text?: Phaser.GameObjects.Text;
-    // platform?: Phaser.Types.Physics.Arcade.ImageWithStaticBody;
-
-    preload() {
-        // this.load.image('star', 'favicon.ico');
+      super({ key: 'MainScene' });
     }
 
     create() {
-    //     this.text = this.add.text(100, 100, "MainScene", {
-    //         font: "24px Courier",
-    //         color: "#ffffff",
-    //     });
-    //     this.platform = this.physics.add.staticImage(0, 200, 'star').setOrigin(0, 0).refreshBody();
+      this._player = this.physics.add.existing(new Player(this, 100, Math.PI));
+      this._cursorKeys = this.input.keyboard?.createCursorKeys();
+      // this.anims.create({
+      //     key: "ring_anim",
+      //     frames: this.anims.generateFrameNumbers('ring'),
+      //     frameRate: 30,
+      //     repeat: -1 
+      // });
+
+      // this._player.play("ring_anim");
     }
 
-    update() {
-        // function incHex(hexStr : string) : string {
-        //     const decNum : integer = parseInt(hexStr.slice(1), 16) + 1;
-        //     return "#"+("000000"+decNum.toString(16)).slice(-6);
-        // }
-        // this.text?.style.setColor(incHex(this.text?.style.color as string));
+    update(time: number, delta: number): void {
+      this._ticks++;
+      this.handleKeyboard();
+      this.shoot();
+    }
+
+    handleKeyboard()
+    {
+      let rad = 0;
+      let ang = 0;
+      if (this._cursorKeys?.left.isDown) ang += Math.PI / 128;
+      if (this._cursorKeys?.right.isDown) ang -= Math.PI / 128;
+      if (this._cursorKeys?.up.isDown) rad += 5;
+      if (this._cursorKeys?.down.isDown) rad -= 5;
+      this._player?.Move(rad, ang);
+    }
+
+    shoot()
+    {
+      if (this._ticks % 10 == 0) 
+      {
+        const bullet = new PlayerBullet(this);
+        bullet.play('bullet_anim');
+      }
+
+      this._projectiles.forEach(element => {
+        element.Move();
+      });
     }
   }
